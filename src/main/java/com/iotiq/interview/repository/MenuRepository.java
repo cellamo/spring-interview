eabe4bce1b90e33c390d611defcd2f4eca4c2607
inci <inci@iotiq.net>
2024-07-04 21:20:35 +0300

    some changes

--- src/main/java/com/iotiq/interview/repository/MenuRepository.java
+++ src/main/java/com/iotiq/interview/repository/MenuRepository.java
@@ -3,7 +3,9 @@ package com.iotiq.interview.repository;
 import com.iotiq.interview.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MenuRepository extends JpaRepository<Menu, UUID> {
+    List<Menu> findAllByCategory(String name);
 }
