0b6c8ccec5700f52d69df696281c5b74fd48c446
Oguz Kurt <oguz@iotiq.net>
2024-07-05 00:41:29 +0300

    Add getter to Dto class

--- src/main/java/com/iotiq/interview/controller/messages/CreateResponse.java
+++ src/main/java/com/iotiq/interview/controller/messages/CreateResponse.java
@@ -1,10 +1,12 @@
 package com.iotiq.interview.controller.messages;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.UUID;
 
 @Builder
+@Getter
 public class CreateResponse {
     private UUID id;
 }
