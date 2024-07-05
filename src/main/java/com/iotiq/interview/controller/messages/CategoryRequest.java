08ae3588f25e92ba09c9effecdfd901c083412f0
inci <inci@iotiq.net>
2024-07-05 09:40:34 +0300

    some changes

--- src/main/java/com/iotiq/interview/controller/messages/CategoryRequest.java
+++ src/main/java/com/iotiq/interview/controller/messages/CategoryRequest.java
@@ -1,10 +1,16 @@
 package com.iotiq.interview.controller.messages;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 public class CategoryRequest {
     @NotEmpty
     private String name;
+
+    @NotNull
+    UUID menuId;
 }
--- src/main/java/com/iotiq/interview/controller/messages/CreateResponse.java
+++ src/main/java/com/iotiq/interview/controller/messages/CreateResponse.java
@@ -1,9 +1,11 @@
 package com.iotiq.interview.controller.messages;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.UUID;
 
+@Data
 @Builder
 public class CreateResponse {
     private UUID id;
